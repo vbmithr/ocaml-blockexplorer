166cc6bf8490e1caea37fcff0ab186b0d12b2d29
Vincent Bernardoff <vb@luminar.eu.org>
2017-04-25 11:42:46 +0200

    async: use Base58

--- async/blockexplorer_async.ml
+++ async/blockexplorer_async.ml
@@ -8,7 +8,7 @@ open Blockexplorer.Types
 exception Client of string
 exception Server of string
 exception API of string
-exception Data_encoding of Ezjsonm.t
+exception Data_encoding of string
 
 let safe_get ?headers ~encoding url =
   Monitor.try_with ~extract_exn:true begin fun () ->
@@ -22,7 +22,10 @@ let safe_get ?headers ~encoding url =
     let json = Ezjsonm.from_string body_str in
     try
       Json_encoding.destruct encoding json
-    with _ -> raise (Data_encoding json)
+    with exn ->
+      let pp = Json_encoding.print_error ?print_unknown:None in
+      let str = Format.asprintf "%a" pp exn in
+      raise (Data_encoding str)
   end >>| Result.map_error ~f:begin function
     | Client str -> Http.Client str
     | Server str -> Server str
@@ -41,7 +44,10 @@ let safe_post ?headers ~params ~encoding url =
     let json = Ezjsonm.from_string body_str in
     try
       Json_encoding.destruct encoding json
-    with _ -> raise (Data_encoding json)
+    with exn ->
+      let pp = Json_encoding.print_error ?print_unknown:None in
+      let str = Format.asprintf "%a" pp exn in
+      raise (Data_encoding str)
   end >>| Result.map_error ~f:begin function
     | Client str -> Http.Client str
     | Server str -> Http.Server str
@@ -69,7 +75,7 @@ let utxos ?(testnet=false) = function
   | addrs ->
       let url = if testnet then testnet_base_url else base_url in
       let url = Uri.with_path url "/api/addrs/utxo" in
-      let params = ["addrs", addrs] in
+      let params = ["addrs", List.map addrs ~f:Base58.to_string] in
       safe_post ~params ~encoding:Json_encoding.(list Utxo.encoding) url
 
 let broadcast_tx ?(testnet=false) (`Hex rawtx_hex) =
@@ -82,7 +88,7 @@ let broadcast_tx ?(testnet=false) (`Hex rawtx_hex) =
 let tx_by_addr ?(testnet=false) addr =
   let url = if testnet then testnet_base_url else base_url in
   let url = Uri.with_path url "/api/txs" in
-  let url = Uri.with_query url ["address", [addr]] in
+  let url = Uri.with_query url ["address", [Base58.to_string addr]] in
   let encoding =
     let open Json_encoding in
     conv (fun s -> (0, s)) (fun (_, s) -> s)
--- async/blockexplorer_async.mli
+++ async/blockexplorer_async.mli
@@ -11,10 +11,10 @@ val rawtx :
   ?testnet:bool -> Hex.t -> Hex.t Http.result Deferred.t
 
 val utxos :
-  ?testnet:bool -> string list -> (Utxo.t list) Http.result Deferred.t
+  ?testnet:bool -> Base58.t list -> (Utxo.t list) Http.result Deferred.t
 
 val broadcast_tx :
   ?testnet:bool -> Hex.t -> Hex.t Http.result Deferred.t
 
 val tx_by_addr :
-  ?testnet:bool -> string -> Tx.t list Http.result Deferred.t
+  ?testnet:bool -> Base58.t -> Tx.t list Http.result Deferred.t
